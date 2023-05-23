656025a33f55bf3496f1d12c90aee0a6246a500a
Marc-Antoine CLAUDE <claude.marcantoine2@gmail.com>
2023-05-23 23:43:07 +0200

    init react-query + devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
 
 import './styles/reset.scss';
 import './styles/index.scss';
 import App from './App';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
