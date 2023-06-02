8bc1b6e8a02df71dd62fd625cf16f983c6c5cdcb
Marc-Antoine CLAUDE <claude.marcantoine2@gmail.com>
2023-06-02 12:04:53 +0200

    authentication context refactor

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -1,9 +1,19 @@
-import { ReactNode, createContext, useContext, useState } from 'react';
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
 type AuthContextType = {
   authenticated: boolean;
-  login: () => void;
+  id: string;
+  token: string;
+  login: (userId: string, token: string) => void;
   logout: () => void;
+  setUserId: (userId: string) => void;
+  setUserToken: (userToken: string) => void;
 };
 
 type AuthProviderProps = {
@@ -12,8 +22,12 @@ type AuthProviderProps = {
 
 const initialAuthContext: AuthContextType = {
   authenticated: false,
+  id: '',
+  token: '',
   login: () => {},
   logout: () => {},
+  setUserId: () => {},
+  setUserToken: () => {},
 };
 
 // Create the authentication context
@@ -21,16 +35,37 @@ export const AuthContext = createContext(initialAuthContext);
 
 // Create the authentication provider component
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [id, setId] = useState('');
-  const [token, setToken] = useState('');
+  const storedAuthCredentials = JSON.parse(
+    sessionStorage.getItem('authCredentials')!,
+  );
 
-  const login = () => {
+  const getAuthenticated = () => storedAuthCredentials?.authenticated;
+  const getId = () => storedAuthCredentials?.id;
+  const getToken = () => storedAuthCredentials?.token;
+
+  const [authenticated, setAuthenticated] = useState(getAuthenticated());
+  const [id, setId] = useState(getId());
+  const [token, setToken] = useState(getToken());
+
+  useEffect(() => {
+    // INFO: For dev purpose store auth credentials in session storage
+    //       -> TODO: find a more secure way for production
+    sessionStorage.setItem(
+      'authCredentials',
+      JSON.stringify({ authenticated, id, token }),
+    );
+  }, [authenticated]);
+
+  const login = (userId: string, userToken: string) => {
     setAuthenticated(true);
+    setId(userId);
+    setToken(userToken);
   };
 
   const logout = () => {
     setAuthenticated(false);
+    setId('');
+    setToken('');
   };
 
   const setUserId = (userId: string) => {
